c3068e63f71520cf3c4d50852669cbf89ea7cde3
zach <zach.moore0628@gmail.com>
2024-01-19 19:16:35 -0600

    Feature complete

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,38 @@
+class Node:
+    def __init__(self,value,next=None):
+        self.value = value
+        self.next = next
+
+class InvalidOperationError(Exception):
+    pass
+
 class Queue:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.front = None
+        self.back = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.back is None:
+            self.front = new_node
+            self.back = new_node
+        else:
+            self.back.next = new_node
+            self.back = new_node
+
+    def dequeue(self):
+        if self.front is None:
+            raise InvalidOperationError("Method not allowed on empty queue.")
+        dequeue_value = self.front.value
+        self.front = self.front.next
+        if self.front is None:
+            self.back = None
+        return dequeue_value
+
+    def peek(self):
+        if self.front is None:
+            raise InvalidOperationError("Method not allowed on empty queue.")
+        return self.front.value
+
+    def is_empty(self):
+        return self.front is None
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,30 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class InvalidOperationError(Exception):
+    pass
+
 class Stack:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        new_node = Node(value, self.top)
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        pop_value = self.top.value
+        self.top = self.top.next
+        return pop_value
+
+    def peek(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
