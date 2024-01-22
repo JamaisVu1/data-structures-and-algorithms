5822e12c2757a84128fca70499d04eddcc3cf5e8
zach <zach.moore0628@gmail.com>
2024-01-22 15:23:21 -0600

    feature complete

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,16 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    
+    def __init__(self):
+        self.enqueue_stack = Stack()
+        self.dequeue_stack = Stack()
+        
+    def enqueue(self, value):
+        self.enqueue_stack.push(value)
+        
+    def dequeue(self):
+        if self.dequeue_stack.is_empty():
+            while not self.enqueue_stack.is_empty():
+                self.dequeue_stack.push(self.enqueue_stack.pop())
+        return self.dequeue_stack.pop()
---
+++ python/docs/stack_queue_pseudo/README.md
@@ -0,0 +1,12 @@
+# stack-queue-pseudo
+Use two instances of a stack to create a queue
+
+## Whiteboard Process
+
+[Whiteboard](pseudo.png)
+
+## Approach & Efficiency
+
+I realized putting the values from one stack to another would mimic dequeue when popping from the second stack.
+
+## Solution
