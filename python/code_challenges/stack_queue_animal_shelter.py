5cbadfabfe3ff8821d0b802e4690438d6c2ed6a0
zach <zach.moore0628@gmail.com>
2024-01-23 16:40:22 -0600

    feature complete

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,39 @@
 from data_structures.queue import Queue
 
-
-class AnimalShelter:
-    pass
-
-
 class Dog:
-    pass
-
+    def __init__(self, name=None):
+        self.species = "dog"
+        self.name = name if name is not None else "no name"
 
 class Cat:
-    pass
+    def __init__(self, name=None):
+        self.species = "cat"
+        self.name = name if name is not None else "no name"
+
+class AnimalShelter:
+    def __init__(self):
+        self.animals = Queue()
+
+    def enqueue(self, animal):
+        if not isinstance(animal, (Dog, Cat)):
+            raise ValueError("must be a Dog or Cat")
+        self.animals.enqueue(animal)
+
+    def dequeue(self, pref):
+        if pref not in ["dog", "cat"]:
+            return None
+
+        adopted = None
+        temp = []
+
+        while not self.animals.is_empty():
+            animal = self.animals.dequeue()
+            if adopted is None and animal.species == pref:
+                adopted = animal
+            else:
+                temp.append(animal)
+
+        for animal in temp:
+            self.animals.enqueue(animal)
+
+        return adopted
