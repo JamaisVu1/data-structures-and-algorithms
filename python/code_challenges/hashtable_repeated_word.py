f2f95ce7d25673f3289ebfa4cc39bdac4a9a6bfa
zach <zach.moore0628@gmail.com>
2024-02-19 15:39:52 -0600

    feature complete

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,17 @@
 from data_structures.hashtable import Hashtable
 
-
-def first_repeated_word():
-    pass
+# GPT Assisted
+def first_repeated_word(input_string):
+    normalized_string = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in input_string.lower())
+    
+    words = normalized_string.split()
+    
+    hashtable = Hashtable()
+    
+    for word in words:
+        if word:  
+            if hashtable.has(word):
+                return word
+            hashtable.set(word, True)
+    
+    return None
