336d2ee98ff50dfc9e6d27f6e55e9cb5f1d90cd3
zach <zach.moore0628@gmail.com>
2024-02-21 16:25:11 -0600

    failed

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,10 @@
 from data_structures.hashtable import Hashtable
 
-
-def left_join():
-    pass
+# GPT assisted
+def left_join(synonyms, antonyms):
+    result = []
+    for key in synonyms.keys():
+        synonym = synonyms.get(key)
+        antonym = antonyms.get(key) if antonyms.contains(key) else "NONE"
+        result.append([key, synonym, antonym])
+    return result
